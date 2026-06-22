e2d5cb7240d67128fd764bff1a612b6ca4529e40
Ashley S. Harris <ashley@orynth.net>
2026-06-22 17:01:33 -0500

    Add AOMS metrics engine

---
+++ src/metrics.py
@@ -0,0 +1,24 @@
+def generate_metrics(results):
+    total_cases = len(results)
+    total_declined = sum(1 for r in results if r["execution_result"] == "DECLINED")
+    total_approved = sum(1 for r in results if r["execution_result"] == "APPROVED")
+    total_admissible = sum(1 for r in results if r["admissibility"] is True)
+    total_inadmissible = sum(1 for r in results if r["admissibility"] is False)
+
+    return {
+        "total_cases": total_cases,
+        "total_approved": total_approved,
+        "total_declined": total_declined,
+        "total_admissible": total_admissible,
+        "total_inadmissible": total_inadmissible
+    }
+
+
+def print_metrics(metrics):
+    print("")
+    print("AOMS Metrics")
+    print(f"Total Cases: {metrics['total_cases']}")
+    print(f"Approved Executions: {metrics['total_approved']}")
+    print(f"Declined Executions: {metrics['total_declined']}")
+    print(f"Admissible Cases: {metrics['total_admissible']}")
+    print(f"Inadmissible Cases: {metrics['total_inadmissible']}")
